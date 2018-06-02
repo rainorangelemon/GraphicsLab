7f228fff642c48427a347c5d4915ae710a49b4df
rainorangelemon <rainorangelemon@gmail.com>
2018-06-02 16:00:05 +0800

    solve the conflict between fill and clip

--- src/model/ModelFill.java
+++ src/model/ModelFill.java
@@ -10,6 +10,7 @@ import javafx.util.Pair;
 public class ModelFill extends ModelShape{
 	private int seedX, seedY;
 	private int width, height;
+	private int x0, y0;
 	private boolean connectivityFour, isInteriorPointMethod;
 	private Color definedColor;
 	private List<Pair<Integer, Integer>> fourNeighbours = new ArrayList<Pair<Integer, Integer>>(){
@@ -35,11 +36,13 @@ public class ModelFill extends ModelShape{
 		}
 	};
 	
-	public ModelFill(boolean connectivityFour, int width, int height, int seedX, int seedY, 
+	public ModelFill(boolean connectivityFour, int x0, int y0, int width, int height, int seedX, int seedY, 
 			Color targetColor, boolean isInteriorPointMethod, Color definedColor){
 		super(targetColor);
 		this.width = width;
 		this.height = height;
+		this.x0 = x0;
+		this.y0 = y0;
 		this.seedX = seedX;
 		this.seedY = seedY;
 		this.connectivityFour = connectivityFour;
@@ -92,10 +95,10 @@ public class ModelFill extends ModelShape{
 
 	@Override
 	protected List<ModelDot> getModelDots(ModelDot[][] dots) {
-		boolean[][] visit = new boolean[height][width];
+		boolean[][] visit = new boolean[dots.length][dots[0].length];
 		List<ModelDot> result = new ArrayList<ModelDot>();
 		Stack<ModelDot> waitingStack = new Stack<ModelDot>();
-		if(((width>seedX)&&(seedX>=0))&&((height>seedY)&&(seedY>=0))){
+		if((((width+x0)>seedX)&&(seedX>=x0))&&(((height+y0)>seedY)&&(seedY>=y0))){
 			waitingStack.push(dots[seedY][seedY]);
 			if(isInteriorPointMethod){
 				definedColor = dots[seedY][seedX].getColor();
@@ -119,8 +122,8 @@ public class ModelFill extends ModelShape{
 			neighbourOffSet = eightNeighbours;
 		}
 		for(Pair<Integer, Integer> offset: neighbourOffSet){
-			if((seedDot.getX() + offset.getKey()<width)&&(seedDot.getX() + offset.getKey()>=0)
-					&&(seedDot.getY() + offset.getValue()<height)&&(seedDot.getY() + offset.getValue()>=0)){
+			if((seedDot.getX() + offset.getKey()<(width+x0))&&(seedDot.getX() + offset.getKey()>=x0)
+					&&(seedDot.getY() + offset.getValue()<(height+y0))&&(seedDot.getY() + offset.getValue()>=y0)){
 				candidates.add(dots[seedDot.getY() + offset.getValue()][seedDot.getX() + offset.getKey()]);
 			}
 		}
@@ -158,10 +161,48 @@ public class ModelFill extends ModelShape{
 		return this;
 	}
 
-
-
+	// 第二大元素
+	private int find2Max(int[] a){  
+	    int max1 = 0;     
+	    int max2 = 0;    
+	    for(int i=1; i< a.length; i++)    
+	    {    
+	        if(a[i] > a[max1]) {    
+	            max2 = max1;    
+	            max1 = i;    
+	        } else if(a[i] >  a[max2] && a[i] < a[max1]){
+	            max2 = i;    
+	        }
+	    }  
+	    return a[max2];  
+	} 
+	
+	// 第二小元素
+	private int find2Min(int[] a){  
+	    int min1 = 0;     
+	    int min2 = 0;    
+	    for(int i=1; i< a.length; i++)    
+	    {    
+	        if(a[i] < a[min1]) {    
+	            min2 = min1;    
+	            min1 = i;    
+	        } else if(a[i] <  a[min2] && a[i] > a[min1]){
+	            min2 = i;    
+	        }
+	    }  
+	    return a[min2];  
+	} 
+	
 	@Override
 	public ModelShape clip(int windowX0, int windowY0, int windowX1, int windowY1) {
-		return this;
+//		if((windowX0>(x0+width))||(windowX1<x0)||(windowY0>(y0+height))||(windowY1<y0)){
+//			return new ModelDots(new ArrayList<ModelDot>());
+//		}else{
+			int minX = find2Min(new int[]{windowX0, windowX1, x0, x0+width});
+			int minY = find2Min(new int[]{windowY0, windowY1, y0, y0+height});
+			int maxX = find2Max(new int[]{windowX0, windowX1, x0, x0+width});
+			int maxY = find2Max(new int[]{windowY0, windowY1, y0, y0+height});
+			return new ModelFill(connectivityFour, minX, minY, maxX-minX+1, maxY-minY+1, seedX, seedY, super.getColor(), isInteriorPointMethod, definedColor);
+//		}
 	}
 }
